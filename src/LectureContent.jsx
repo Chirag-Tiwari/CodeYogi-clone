065bebe7051c595a050094480f03e2f0805e8f74
Chirag Tiwari <tiwarict388@gmail.com>
2022-03-31 05:27:04 +0530

    have done some changes in lecturecontent.jsx and lectureList.jsx

--- src/LectureContent.jsx
+++ src/LectureContent.jsx
@@ -7,7 +7,7 @@ function LectureContent (props){
       <h1 className="font-medium">Lecture #{props.id} <span className="font-medium text-gray-500">({props.date})</span></h1>
       <h1 className="text-gray-500">Duration: {props.duration}</h1>
       <ul className="list-disc ml-6">
-        <li>hello</li>
+        <li>{props.topics}</li>
       </ul>
       <div className="flex justify-center">
         <a className="text-gray-500 font-medium" href={props.link}>Watch/Download Recording</a>
