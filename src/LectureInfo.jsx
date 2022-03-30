e4dcc3e23a8d576aefd6e657f5f3328c9da96c36
Chirag Tiwari <tiwarict388@gmail.com>
2022-03-31 05:21:28 +0530

    added topics in lectureInfo.jsx

--- src/LectureInfo.jsx
+++ src/LectureInfo.jsx
@@ -3,30 +3,34 @@ export const lectureDetail=[
     date:"Fri Mar 11 2022",
      duration:"03:25:24",
      link:"https://google.com",
-     topics:"how to use images in react app "
+     topics:"how to use images in react app ","how to use git","how are you hello ","kya hori hai bhai "
       },
     
     {id:"2",
     date:"Mon Mar 14 2022",
      duration:"03:07:08",
-     link:"https://google.com"
+     link:"https://google.com",
+     topics:"how to use images in react app ","how to use git","how are you hello ","kya hori hai bhai "
      },
   
      {id:"3",
     date:"Tue Mar 14 2022",
      duration:"03:35:42",
-      link:"https://google.com"
+      link:"https://google.com",
+      topics:"how to use images in react app ","how to use git","how are you hello ","kya hori hai bhai "
      },
   
      {id:"4",
     date:"Wed Mar 16 2022",
      duration:"03:32:01",
-      link:"https://google.com"
+      link:"https://google.com",
+      topics:"how to use images in react app ","how to use git","how are you hello ","kya hori hai bhai "
      },
   
      {id:"5",
     date:"Mon Mar 21 2022",
      duration:" 03:00:23",
-      link:"https://google.com"
+      link:"https://google.com",
+      topics:"how to use images in react app ","how to use git","how are you hello ","kya hori hai bhai "
      }
   ]
