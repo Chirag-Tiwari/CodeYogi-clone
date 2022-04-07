7b3cd21b8143b7b9c6106793f285159407246f9a
Chirag Tiwari <tiwarict388@gmail.com>
2022-04-07 16:27:17 +0530

    added icon in student link

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { MdOutlineQuiz,MdOutlineAssignment } from 'react-icons/md';
 import { GiTeacher } from 'react-icons/gi';
+import { IoIosSchool } from "react-icons/io"
 
 function SideBar (){
     return(
@@ -10,7 +11,7 @@ function SideBar (){
   <Link to= "/lecture" className="text-white text-bold flex hover:bg-gray-900"><span className="text-2xl mr-2"><GiTeacher/></span>Lecture</Link>
   <Link to= "/assignment" className="text-white text-bold flex hover:bg-gray-900"> <span className=" mr-2 text-2xl"><MdOutlineAssignment/> </span>Assignment</Link>
   <Link to="/quiz" className="text-white text-bold flex hover:bg-gray-900"><span className=" mr-2 text-2xl"><MdOutlineQuiz/></span> Quiz</Link>
-  <Link to="/students" className="text-white text-bold flex hover:bg-gray-900">Students</Link>
+  <Link to="/students" className="text-white text-bold flex hover:bg-gray-900"><span className=" mr-2 text-2xl"><IoIosSchool/></span> Students</Link>
  </div>
     );
 }
