72d1dc7d05ba7abb3954c168360651a574e2aba1
Chirag Tiwari <tiwarict388@gmail.com>
2022-04-07 12:16:58 +0530

    have done several changes in clone

--- src/StudentLayout.jsx
+++ src/StudentLayout.jsx
@@ -3,12 +3,12 @@ import { GrTwitter } from "react-icons/gr";
 import {AiFillLinkedin} from "react-icons/ai";
 import Card from "./Card"
 
-function StudentLayout(students){
+function StudentLayout(){
     return(
         <div className=" bg-white m-3 w-72 h-72">
            <img className="h-52 w-72 object-cover rounded-lg" src="https://images.unsplash.com/photo-1649003175381-2df7e82ef6f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"/>
           <div>
-           <h1 className="font-bold text-xl">{students}</h1>
+           <h1 className="font-bold text-xl">hello</h1>
            <h1 className="font-bold text-indigo-500">Front-end Developer</h1>
            <div className="flex space-x-4 text-gray-500 text-xl">
            <a href="https://twitter.com"><GrTwitter/></a>
