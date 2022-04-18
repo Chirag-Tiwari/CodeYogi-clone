09099c06d8d3004518af6cd537d5c4d1e713cc9a
Chirag Tiwari <tiwarict388@gmail.com>
2022-04-18 05:52:29 +0530

    used useNavigate in assignmentContent file

--- src/AssignmentContent.jsx
+++ src/AssignmentContent.jsx
@@ -1,18 +1,23 @@
-import React from "react"
-import Card from "./Card"
-import { Link } from "react-router-dom"
-import { RiCheckboxCircleLine } from "react-icons/ri"
-import { HiOutlineExternalLink } from "react-icons/hi"
-import AssignmentSubmissionPopUp from "./AssignmentSubmissionPopUp"
+import React from "react";
+import Card from "./Card";
+import { Link } from "react-router-dom";
+import { RiCheckboxCircleLine } from "react-icons/ri";
+import { HiOutlineExternalLink } from "react-icons/hi";
+import AssignmentSubmissionPopUp from "./AssignmentSubmissionPopUp";
+import { useNavigate } from "react-router-dom"
 
 function AssignmentContent({ assignment }) {
 
+  //to={`/assignment/${assignment.id}/details`}
+
   const [showPopUp, updateShowPopUp] = React.useState(false);
+
+  const navigate = useNavigate()
 
   return (
     <div>
       <Card>
-        <Link to={`/assignment/${assignment.id}/details`}>
+        <div onClick={() => navigate(`/assignment/${assignment.id}/details`)}>
           <h3 className="font-medium"># {assignment.id} {assignment.title}<span className="text-gray-500">({assignment.created_at})</span></h3>
           <div className="flex justify-between">
             <div className="my-2">
@@ -21,9 +26,9 @@ function AssignmentContent({ assignment }) {
             </div>
             <h1 className="font-semibold text-green-600">Submitted</h1>
           </div>
-        </Link>
+        </div>
         <div className="flex justify-center">
-          <button className="text-green-600 font-medium border-r-2 border-gray-400 px-24 flex" onClick={() => { updateShowPopUp(true) }}><span className="text-2xl mr-2"><RiCheckboxCircleLine /></span>Re-Submitt</button>
+          <button className="text-green-600 font-medium border-r-2 border-gray-400 px-24 flex" onClick={() => { updateShowPopUp(!showPopUp) }}><span className="text-2xl mr-2"><RiCheckboxCircleLine /></span>Re-Submitt</button>
           <a className="text-indigo-500 font-medium mx-24 flex underline underline-offset-2" href={assignment.submissions.submission_link}><span className="text-xl mt-1"><HiOutlineExternalLink /></span>See Your Submission</a>
         </div>
       </Card>
--- src/AssignmentSubmissionPopUp.jsx
+++ src/AssignmentSubmissionPopUp.jsx
@@ -2,15 +2,21 @@ import React from "react";
 import Button from "./Button";
 
 function AssignmentSubmissionPopUp() {
+    const [assignmentUrl, setAssignmentUrl] = React.useState('')
+
+    const submitAssignment = (event) => {
+        setAssignmentUrl(event.target.value)
+        axios.put(`https://api.codeyogi.io/${assignment.id}/submit`, { submissionLink: assignmentUrl }, { withCredentials: true })
+    }
     return (
         <div className="bg-white p-4 rounded-md w-2/5 h-44 border border-gray-600 fixed  items-center ">
             <div className="flex flex-col">
                 <div className="flex border-y border-gray-200 py-5">
                     <h2 className="px-3 py-1 font-medium text-gray-500">Submission Link</h2>
-                    <input className="w-full px-3 py-1 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-r-md" placeholder="Submission Link" type="text" />
+                    <input value={assignmentUrl} onChange={submitAssignment} className="w-full px-3 py-1 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-r-md" placeholder="Submission Link" type="text" />
                 </div>
                 <div className="py-4">
-                    <Button onClick>Submit</Button>
+                    <Button>Submit</Button>
                 </div>
             </div>
         </div>
